81c0437273be129bd568824b1e289b5731f7b0c8
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2024-07-29 14:24:22 +0000

    Make check_and_update_readiness reusable from inside a FIleDescription

--- src/shims/unix/fd.rs
+++ src/shims/unix/fd.rs
@@ -250,31 +250,9 @@ impl FileDescriptionRef {
         &self,
         ecx: &mut InterpCx<'tcx, MiriMachine<'tcx>>,
     ) -> InterpResult<'tcx, ()> {
-        // Get a list of epoll_event that is associated to a specific file description.
-        if let Some(epoll_interests) = ecx.machine.epoll_interests.get_epoll_interest(self.get_id())
-        {
-            // Retrieve the readiness events of the file description.
-            let ready_events = self.borrow_mut().get_epoll_ready_events(ecx)?;
-
-            for weak_epoll_interest in epoll_interests {
-                if let Some(epoll_interest) = weak_epoll_interest.upgrade() {
-                    // This checks if any of the events specified in epoll_interest.events match those
-                    // in ready_events.
-                    let epoll_interest = epoll_interest.borrow();
-                    let flags = epoll_interest.events & ready_events;
-                    // If there is any event that we are interested in being specified as ready,
-                    // insert a epoll_return to the ready list.
-                    if flags != 0 {
-                        let weak_fd_ref = epoll_interest.weak_file_description_ref.clone();
-                        let epoll_key = (weak_fd_ref, epoll_interest.file_descriptor);
-                        let ready_list = &mut epoll_interest.ready_list.borrow_mut();
-                        let epoll_return = EpollReturn::new(flags, epoll_interest.data);
-                        ready_list.insert(epoll_key, epoll_return);
-                    }
-                }
-            }
-        }
-        Ok(())
+        ecx.check_and_update_readiness(self.get_id(), |ecx| {
+            self.borrow_mut().get_epoll_ready_events(ecx)
+        })
     }
 }
 
@@ -413,6 +391,39 @@ impl FdTable {
 
 impl<'tcx> EvalContextExt<'tcx> for crate::MiriInterpCx<'tcx> {}
 pub trait EvalContextExt<'tcx>: crate::MiriInterpCxExt<'tcx> {
+    /// For a specific unique file descriptor id, get its ready events and update the corresponding ready lists
+    fn check_and_update_readiness(
+        &self,
+        id: FdID,
+        f: impl FnOnce(&MiriInterpCx<'tcx>) -> InterpResult<'tcx, u32>,
+    ) -> InterpResult<'tcx, ()> {
+        let this = self.eval_context_ref();
+        // Get a list of epoll_event that is associated to a specific file description.
+        if let Some(epoll_interests) = this.machine.epoll_interests.get_epoll_interest(id) {
+            // Retrieve the readiness events of the file description.
+            let ready_events = f(this)?;
+
+            for weak_epoll_interest in epoll_interests {
+                if let Some(epoll_interest) = weak_epoll_interest.upgrade() {
+                    // This checks if any of the events specified in epoll_interest.events match those
+                    // in ready_events.
+                    let epoll_interest = epoll_interest.borrow();
+                    let flags = epoll_interest.events & ready_events;
+                    // If there is any event that we are interested in being specified as ready,
+                    // insert a epoll_return to the ready list.
+                    if flags != 0 {
+                        let weak_fd_ref = epoll_interest.weak_file_description_ref.clone();
+                        let epoll_key = (weak_fd_ref, epoll_interest.file_descriptor);
+                        let ready_list = &mut epoll_interest.ready_list.borrow_mut();
+                        let epoll_return = EpollReturn::new(flags, epoll_interest.data);
+                        ready_list.insert(epoll_key, epoll_return);
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
+
     fn dup(&mut self, old_fd: i32) -> InterpResult<'tcx, i32> {
         let this = self.eval_context_mut();
 
